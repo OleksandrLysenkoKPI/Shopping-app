f80887a558c83f6f7eb4d7620fa501c6fb8dd6ed
OleksandrLysenkoKPI <oleksandrlysenkotb23@gmail.com>
2025-12-06 21:21:42 +0200

    Created bottom navigation bar on home screen

---
+++ app/src/main/java/com/example/shoppingapp/pages/CartPage.kt
@@ -0,0 +1,10 @@
+package com.example.shoppingapp.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CartPage(modifier: Modifier = Modifier) {
+    Text("Cart page")
+}
