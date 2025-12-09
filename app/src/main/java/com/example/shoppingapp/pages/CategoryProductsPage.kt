524b9c220d372d64d2de5e5239a4b1b796bc439c
OleksandrLysenkoKPI <oleksandrlysenkotb23@gmail.com>
2025-12-09 22:11:04 +0200

    Designed category page

--- app/src/main/java/com/example/shoppingapp/pages/CategoryProductsPage.kt
+++ app/src/main/java/com/example/shoppingapp/pages/CategoryProductsPage.kt
@@ -1,11 +1,11 @@
 package com.example.shoppingapp.pages
 
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.shoppingapp.components.ProductItemView
 import com.example.shoppingapp.model.ProductModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -37,13 +38,15 @@ fun CategoryProductsPage(modifier: Modifier = Modifier, categoryId: String) {
             }
     }
 
-    LazyColumn(
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(16.dp),
         modifier = modifier.fillMaxSize()
-            .padding(16.dp)
     ) {
-        items(productsList.value){ item ->
-            Text(item.title + ": " + item.actualPrice)
-            Spacer(modifier = Modifier.height(50.dp))
+        items(productsList.value) { product ->
+            ProductItemView(product = product)
         }
     }
 }
